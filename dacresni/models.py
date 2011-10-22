9ecb496f7ca4ecbdd06b7b723bd8ff32fb71e6d3
Nehemiah I. Dacres <vivacarlie@gmail.com>
2011-10-21 22:54:28 -0500

    added function entity

--- dacresni/models.py
+++ dacresni/models.py
@@ -8,6 +8,14 @@ class Manual(db.Model):
     content = db.TextProperty()
     section = db.IntegerProperty()
     
+class Function(db.Model):
+    name = db.StringProperty(required=True)
+    #params = db.StringProperty()
+    #I've been considering the utility of a parameter list for a while
+    headers = db.StringList(required=Ture)
+    manual = db.Reference(Manual)
+
+
 
 
 
