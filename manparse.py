3046fd1316f714dbac3562aebca6a0326a12a100
Nehemiah I. Dacres <vivacarlie@gmail.com>
2011-11-07 22:27:51 -0600

    renamed manparse folder to mantext  added templates directory and manparse.py stub

---
+++ manparse.py
@@ -0,0 +1,10 @@
+# a file to parse the code of the man pages 
+import re
+# a proseadure to unzip the mac manpages from /share/man/man2 
+def unzip(path):
+    pass
+
+# the following strings are to regular expressions to find 
+date_string=
+author_string=
+
