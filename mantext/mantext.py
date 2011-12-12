efa9b17ade5521fe668415125d62989a56b72993
Nehemiah I. Dacres <vivacarlie@gmail.com>
2011-12-11 23:19:40 -0600

    the correct filter is col -b to parse manpages

--- mantext/mantext.py
+++ mantext/mantext.py
@@ -7,7 +7,7 @@ from subprocess import Popen, PIPE, STDOUT
 def names():
     for page in os.listdir("/usr/share/man/man2/"):
         manname = page.split(".")[0]
-        command = "man 2 %s| col"%(manname) 
+        command = "man 2 %s| col -b"%(manname) 
         print command
         p = Popen(command, shell=True, stdout=PIPE,stderr=STDOUT )
         mantext= p.communicate()[0]
