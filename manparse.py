27f1b1633f5c1c09af2000997cb0d84f0dfc349a
Nehemiah I. Dacres <vivacarlie@gmail.com>
2012-01-06 09:52:27 -0600

    changed dacresni/ to appengine/

--- manparse.py
+++ manparse.py
@@ -1,8 +1,5 @@
 # a file to parse the code of the man pages 
 import re
-# a proseadure to unzip the mac manpages from /share/man/man2 
-def unzip(path):
-    pass
 
 # the following strings are to regular expressions to find 
 date_string=
--- mans/urls.py
+++ mans/urls.py
@@ -8,10 +8,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mans.views.home', name='home'),
     # url(r'^mans/', include('mans.foo.urls')),
-
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^txt/',mans.views.txt),
+    url(r'^dynamic/'),
+    url(r'^static/')
 )
